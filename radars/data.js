afeefc3b05d16d6c4b0861ae9575fefbb18a9bd7
Carlos Villela <cvillela@thoughtworks.com>
2011-05-26 13:45:46 -0300

    making example data anonymous

--- radars/data.js
+++ radars/data.js
@@ -1,54 +1,33 @@
-title = 'Carlos Villela, May 2011';
+title = 'Your Name, Month Year';
 
 data = {
-    'Languages': {
-        'Java': 4,
-        'Python':3,
-        'Objective-C':1,
-        'C':2,
-        'Smalltalk':2,
-        'Ruby': 4,
-        'Clojure':1,
+    'Quadrant': {
+        'Very High Skill': 4,
+        'High Skill': 3,
+        'Medium Skill': 2,
+        'Low Skill': 1,
     },
 
-    'Tools': {
-        'Neo4j': 1,
-        'MongoDB': 3,
-        'Rake': 3,
-        'EC2':  1,
-        'MySQL':3,
-        'PostgreSQL':2,
-        'Maven':2,
-        'Ivy':1,
-        'Oracle':2,
-        'Ant':4,
-        'Capistrano':3,
-        'IntelliJ':2,
-        'Git':3,
-        'Hibernate':2,
-        'Vim':2,
-        'Svn':3,
+    'Quadrant': {
+        'Very High Skill': 4,
+        'High Skill': 3,
+        'Medium Skill': 2,
+        'Low Skill': 1,
     },
 
-    'Techniques': {
-        'TDD': 4,
-        'DDD': 2,
-        'BDD': 4,
-        'Continuous Delivery':3,
-        'REST':3,
-        'OOD': 4,
-        'DevOps':2,
-        'Refactoring':4,
+    'Quadrant': {
+        'Very High Skill': 4,
+        'High Skill': 3,
+        'Medium Skill': 2,
+        'Low Skill': 1,
     },
 
-    'Platforms': {
-        'Linux':4,
-        'MacOS X':4,
-        'Windows':2,
-        'JEE':4,
-        'iOS':1,
-        'Android':1,
-        'JME':1,
-    }
+    'Quadrant': {
+        'Very High Skill': 4,
+        'High Skill': 3,
+        'Medium Skill': 2,
+        'Low Skill': 1,
+    },
+
 };
 
